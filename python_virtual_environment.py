6259ae21fc2f687028eb02527eeba1f3bb6fab17
nithyanandham-thiru-pydev <79956461+nithyanandham-thiru-pydev@users.noreply.github.com>
2021-03-31 09:20:15 +0530

    Add files via upload

---
+++ python_virtual_environment.py
@@ -0,0 +1,37 @@
+
+
+
+Python Virtual Environments
+
+
+
+How to connect create a Python Virtual Environment
+
+
+It is often useful to have one or more Python environments where you can experiment with different combinations of packages without affecting
+your main installation. Python supports this through virtual environments. The virtual environment is a copy of an existing version of Python
+with the option to inherit existing packages. A virtual environment is also useful when you need to work on a shared system and do not have 
+permission to install packages as you will be able to install them in the virtual environment
+
+
+
+
+Outline
+Open a terminal
+Setup the pip package manager
+Install the virtualenv package
+Create the virtual environment
+Activate the virtual environment
+Deactivate the virtual environment
+Optional: Make the virtual environment your default Python
+More: Python virtualenv documentation
+
+
+
+
+Requirements
+
+
+An installation of Python
+
+
